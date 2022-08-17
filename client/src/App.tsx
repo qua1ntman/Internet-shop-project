690f731d1640348e9b88bf86e319efb99d936777
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-17 17:12:28 +0300

    fix: add routes for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
+import { Nav } from './components/Nav';
 
-export const App = () => (
-  <h1>My React and TypeScript App!</h1>
-);
+export const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Nav />
+        <Routes>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+};
