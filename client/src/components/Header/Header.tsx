23e907667c8b949bed95a673b13ada380da31707
Mikhail Ashla <redikfrost@gmail.com>
2022-08-18 13:32:07 +0300

    feat: add header, its' images, update styles

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Search } from './Search/Search'
+import { Link } from 'react-router-dom'
+import './Header.scss'
+import Logo from '../../assets/png/logo.png'
 
 export const Header = () => {
   return (
-    <header>Header</header>
+    <header>
+      <div className='logo'>          
+        <Link to='/'>
+          <img src={Logo} alt="logo" />
+        </Link>
+      </div>
+      <Search />
+      <div className='theme'></div>
+      <div className='lang'></div>
+      <div className='login'></div>
+      <div className='cart'></div>
+    </header>
   )
 }
