b0258c210132fc972fcdad9cb8cd6e9763a13445
Mikhail Ashla <redikfrost@gmail.com>
2022-08-18 22:35:23 +0300

    refactor: add some comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,35 +9,37 @@ import { Header } from './components/Header/Header';
 import { Footer } from "./components/Footer/Footer";
 import { themeBackChanger } from "./helpers/themeStyleChanger";
 
+// Контекст для пропсов, в данном случае для useState хука внутри App
 export const appContext = React.createContext(Object) as unknown as 
-Context<{ theme: string; setTheme: Dispatch<SetStateAction<string>>; }>
+  Context<{ theme: string; setTheme: Dispatch<SetStateAction<string>>; }>
 
+// Установка стиля body в зависимости от темы
+document.body.style.backgroundColor = localStorage.getItem('theme') ? localStorage.getItem('theme') === 'light' ? '' : 'black' : ''
 
 export const App = () => {
 
+  // Хук для изменения темы
   const [theme, setTheme] = React.useState<string>(localStorage.getItem('theme') || 'light')
-
-
-
+  
   return (
     <Router>
       <appContext.Provider value = {{ theme, setTheme }}>
-      <Header />
-      <div className="container">
-        <Nav />
-      </div>
-      <main
-        style={themeBackChanger(theme)}
-      >
+        <Header />
         <div className="container">
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path='/product' element={<Product />} />
-            <Route path='/cart' element={<Cart />} />
-          </Routes>
+          <Nav />
         </div>
-      </main>
-      <Footer />
+        <main
+          style={themeBackChanger(theme)}
+        >
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path='/product' element={<Product />} />
+              <Route path='/cart' element={<Cart />} />
+            </Routes>
+          </div>
+        </main>
+        <Footer />
       </appContext.Provider>
     </Router>
   )
--- client/src/helpers/themeStyleChanger.ts
+++ client/src/helpers/themeStyleChanger.ts
@@ -1,7 +1,9 @@
+// Вернет объект с цветом текста
 export const themeTextChanger = (theme: string) => {
   return theme === 'light' ? { color: 'rgb(20, 20, 20)' } : { color: 'white' }
 }
 
+// Вернет объект с цветом бэкграунда
 export const themeBackChanger = (theme: string) => {
   return theme === 'light' ? { backgroundColor: 'white' } : { backgroundColor: 'rgb(20, 20, 20)' }
 }
