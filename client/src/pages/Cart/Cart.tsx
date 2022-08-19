07f92973d2c93188475111f032a46ae009e5da54
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-19 13:24:15 +0300

    fix: update component

--- client/src/pages/Cart/Cart.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Cart = () => {
-  return (
-    <div className="cart-page">
-      <h2>Cart</h2>
-    </div>
-  )
-}
--- client/src/pages/Product/Product.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export const Product = () => {
-  return (
-    <div className="product-page">
-      <h2>Product</h2>
-    </div>
-  )
-}
