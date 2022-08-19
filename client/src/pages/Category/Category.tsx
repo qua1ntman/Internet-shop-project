2e0b2698173efa1b5d9bb9e30f1cd1af85c974ec
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-19 13:20:56 +0300

    feat: create page category

---
+++ client/src/pages/Category/Category.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Outlet } from "react-router-dom"
+
+export const Category = () => {
+  return (
+    <div className="category-page">
+      <h2>Category</h2>
+      <Outlet />
+    </div>
+  )
+}
