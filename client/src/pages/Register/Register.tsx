ce098e424177936bf4da0afd47c148350d6abc6a
Mikhail Ashla <redikfrost@gmail.com>
2022-08-19 22:34:39 +0300

    refactor: remove warning

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from 'react'
+import React, { useContext, useRef } from 'react'
 import { appContext } from '../../App';
 
 export const Register = () => {
