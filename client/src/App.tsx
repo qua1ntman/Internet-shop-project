1940bf962e6efeea46edad4766b5f1422e50d021
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-19 13:23:24 +0300

    fix: update file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Main } from "./pages/Main/Main";
-import { Product } from "./pages/Product/Product";
-import { Nav } from './components/Nav/Nav';
-import { Cart } from "./pages/Cart/Cart";
 import './App.scss';
+import { Category } from "./pages/Category/Category";
+import { Content } from "./components/Content/Content";
+import { Main } from "./pages/Main/Main";
 
 export const App = () => {
   return (
     <Router>
-      <div className="container">
-        <Nav />
-      </div>
-      <main>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Main />} />
-            <Route path='/product' element={<Product />} />
-            <Route path='/cart' element={<Cart />} />
-          </Routes>
-        </div>
-      </main>
+      <Routes>
+        <Route path='/' element={<Content />}>
+          <Route path="main" element={<Main />} />
+          <Route path="men" element={<Category />} />
+          <Route path="women" element={<Category />} />
+          <Route path="children" element={<Category />} />
+        </Route>
+      </Routes>
     </Router>
   )
 };
