f7c5df6b0006768dcdeb399d141535d97d515502
Mikhail Ashla <redikfrost@gmail.com>
2022-08-22 13:52:38 +0300

    feat: add data rows in card component, move cards container div in separate component

--- client/src/components/ProductCardContainer/ProductCardContainer.tsx
+++ client/src/components/ProductCardContainer/ProductCardContainer.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { ProductCard } from "../ProductCard/ProductCard";
 import { IProduct } from "./../../interfaces/dataInterface";
+import './ProductCardContainer.scss'
 
 export const ProductCardContainer = ({
   products,
 }: {
   products: IProduct[];
 }) => {
-  return (
-    <>
+  return (    
+    <div className="cards-container">
       {products.map((item) => (
         <ProductCard key={item.id} item={item} />
       ))}
-    </>
+    </div>
   );
 };
