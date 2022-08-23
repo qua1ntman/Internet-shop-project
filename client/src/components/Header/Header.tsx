ee325f0914a2e79b62fd7d3464a76bb9783b83b0
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-23 14:27:04 +0300

    feat: add Cart in the Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
 import { Search } from './Search/Search'
 import { Link } from 'react-router-dom'
 import './Header.scss'
 import Logo from '../../assets/png/logo.png'
 import { ThemeController } from '../ThemeController/ThemeController'
+import React from 'react'
+import { useShoppingCart } from '../../pages/Basket/ShoppingCartContext'
 
 export const Header = () => {
+
+  const { openCart, cartQuantity } = useShoppingCart()
+
   return (
     <header>
       <div className='logo'>          
-        <Link to='/m'>
+        <Link to='/main'>
           <img src={Logo} alt='logo' />
         </Link>
       </div>
@@ -19,7 +23,13 @@ export const Header = () => {
       <Link className='login' to='/login'>
         {localStorage.getItem('token') ? 'Exit' : 'Log in'}
       </Link>
-      <div className='cart'></div>
+      <div className='cart' onClick={openCart}>Cart
+        {cartQuantity > 0 && (
+          <span className="badge-circle">
+          {cartQuantity}
+        </span>
+        )}
+      </div>      
     </header>
   )
 }
