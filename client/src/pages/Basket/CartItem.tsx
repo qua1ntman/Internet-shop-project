592e5fbcad57ff89c01da62f730185950d0538cf
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-23 14:41:39 +0300

    feat: create Basket for project

---
+++ client/src/pages/Basket/CartItem.tsx
@@ -0,0 +1,44 @@
+import { useShoppingCart } from "./ShoppingCartContext"
+import storeItems from "./items.json"
+import { formatCurrency } from "./formatCurrency"
+import React from "react"
+
+type CartItemProps = {
+  id: number
+  quantity: number
+}
+
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart()
+  const item = storeItems.find(i => i.id === id)
+  if (item == null) return null
+
+  return (
+    <div className="item">
+      {/* <img
+        src={item.imgUrl}
+        alt={' '}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+      /> */}
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+      </div>
+      <div> {formatCurrency(item.price * quantity)}</div>
+      <button
+        onClick={() => removeFromCart(item.id)}
+      >
+        &times;
+      </button>
+    </div>
+  )
+}
