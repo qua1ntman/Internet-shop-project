d9f95c5aa7d6aa06489e68bff7d1cf2529c81ea2
Olga Malkovich <olga.malkovich@gmail.com>
2022-08-31 19:28:22 +0300

    fix: update Store component

--- client/src/pages/Store/Store.tsx
+++ client/src/pages/Store/Store.tsx
@@ -1,13 +1,11 @@
-import React from "react"
-import { Col, Row } from "react-bootstrap"
-import { StoreItem } from "./StoreItem"
-// import { StoreItem } from "../components/StoreItem"
-import storeItems from "../Basket/items.json"
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "./StoreItem";
+import storeItems from "../Basket/items.json";
 
 export function Store() {
   return (
     <>
-      <h1>Store</h1>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map(item => (
           <Col key={item.id}>
--- client/src/pages/Store/StoreItem.tsx
+++ client/src/pages/Store/StoreItem.tsx
@@ -20,35 +20,36 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
   const quantity = getItemQuantity(id)
 
   return (
+    
     <div className="item-container">
-      {/* <img src={imgUrl} alt={''}>Image</img> */}
+      <img src={imgUrl} alt={''} style={{ width: "125px", height: "75px", objectFit: "cover" }}></img>
       <div className="item-info">
         <p className="item-title">{name}</p>
         <p className="item-price">{formatCurrency(price)}</p>
       </div>
       <div className="mt-auto">
-          {quantity === 0 ? (
-            <button className="button-quantity" onClick={() => increaseCartQuantity(id)}>
-              + Add To Cart
-            </button>
-          ) : (
-            <div
-              className="d-flex align-items-center flex-column"
-              style={{ gap: ".5rem" }}
+        {quantity === 0 ? (
+          <button className="button-quantity" onClick={() => increaseCartQuantity(id)}>
+            + Add To Cart
+          </button>
+        ) : (
+          <div
+            className="d-flex align-items-center flex-column"
+            style={{ gap: ".5rem" }}
             >
               <div className="amount">
                 <button className="minus" onClick={() => decreaseCartQuantity(id)}>-</button>
                 <div>
                   <span className="">{quantity}</span> in cart
-                </div>
-                <button className="plus" onClick={() => increaseCartQuantity(id)}>+</button>
               </div>
-              <button onClick={() => removeFromCart(id)}>
-                Remove
-              </button>
-            </div>
-          )}
+              <button className="plus" onClick={() => increaseCartQuantity(id)}>+</button>
+          </div>
+          <button onClick={() => removeFromCart(id)}>
+            Remove
+          </button>
         </div>
+      )}
     </div>
+  </div>
   )
 }
