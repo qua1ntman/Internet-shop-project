0bec3ade11c0e3735e0cb39c8410d929be21c5d6
Olga Malkovich <olga.malkovich@gmail.com>
2022-09-02 14:38:31 +0300

    feat: create data files

---
+++ client/src/@types/project.ts
@@ -0,0 +1,28 @@
+import { IProject } from "../interfaces/dataInterface";
+
+export const project: IProject[] = [
+  {
+    functionality: "registration",
+    text: "User registration/authorization."
+  },
+  {
+    functionality: "category",
+    text: "The website is categorized by gender, clothing type, manufacturer."
+  },
+  {
+    functionality: "fitering",
+    text: "The website has various filters for product characteristics, as well as sorting."
+  },
+  {
+    functionality: "searching",
+    text: "The site allows you to search for goods by keywords."
+  },
+  {
+    functionality: "chanching-theme",
+    text: "The application has at least 2 themes (light and dark)."
+  },
+  {
+    functionality: "telegram-bot",
+    text: "The user can subscribe to the telegram bot, which will send information about new products."
+  }
+]
